c899d41ff27c1758a2eaf582fa182ca43c436443
Alfredo España <alfespa17@gmail.com>
2022-03-18 14:55:57 -0600

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,6 +10,6 @@ output "storage_account_name" {
 
 output "storage_account_key" {
   value       = azurerm_storage_account.storage.primary_access_key
-  description = "Azure Active Directory Application API URI"
+  description = "Azure storage account access key"
   sensitive   = true
 }
